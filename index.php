3dd50d36196f71959ca0e5c8f0d01c9efd09eba1
Juan <6652831+quadlayers@users.noreply.github.com>
2022-12-02 10:09:31 -0300

    Fix Load namespace

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  * @package     WooCommerce License Manager Client
  * @copyright   Copyright (c) 2022, QuadLayers
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU General Public License Version 3
- * @since       1.0.0
+ * @since       0.1.2
  */
 
 require_once 'src/LicenseClient/Load.php';
--- src/LicenseClient/Load.php
+++ src/LicenseClient/Load.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace LicenseClient;
+namespace QuadLayers\LicenseClient;
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 
