d66916478bd201f41e9dd758796c04722eb822dc
Juan <6652831+quadlayers@users.noreply.github.com>
2023-02-03 11:32:55 -0300

    Update composer

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'quadlayers/wp-license-client',
-        'pretty_version' => '1.3.4',
-        'version' => '1.3.4.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '753a1093fc5c095c38dc62e8dd2fcbe8a871a571',
         'type' => 'jetpack-library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'quadlayers/wp-license-client' => array(
-            'pretty_version' => '1.3.4',
-            'version' => '1.3.4.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '753a1093fc5c095c38dc62e8dd2fcbe8a871a571',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
