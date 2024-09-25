e669394d70ebe1d4447588f0537e3116425242b8
Francisco <6652831+franmastromarino@users.noreply.github.com>
2024-09-25 16:22:51 +0200

    feat: add purchase button to notice

--- src/Backend/Notice/Load.php
+++ src/Backend/Notice/Load.php
@@ -30,9 +30,7 @@ class Load {
 			return;
 		}
 
-		$license_menu_url = $this->plugin->get_menu_license_url();
-
-		if ( ! $license_menu_url ) {
+		if ( ! $this->plugin->get_menu_license_url() ) {
 			return;
 		}
 
@@ -45,53 +43,27 @@ class Load {
 						<br/>
 						<?php esc_html_e( 'Please complete the license activation process to receive automatic updates and enable all premium features.', 'wp-license-client' ); ?>
 					</p>
-					<a href="<?php echo esc_url( $license_menu_url ); ?>" class="button-primary">
-						<?php esc_html_e( 'Activate', 'wp-license-client' ); ?>
-					</a>
-					<?php if ( $this->plugin->get_license_key_url() ) : ?>
-						<a href="<?php echo esc_url( $this->plugin->get_license_key_url() ); ?>" class="button-secondary" target="_blank">
-							<?php esc_html_e( 'Get license key', 'wp-license-client' ); ?>
+					<span style="display:flex;align-items:center;gap: 15px;">
+						<a href="<?php echo esc_url( $this->plugin->get_menu_license_url() ); ?>" class="button-primary">
+							<?php esc_html_e( 'Activate', 'wp-license-client' ); ?>
 						</a>
-					<?php endif; ?>
-					<?php if ( $this->plugin->get_support_url() ) : ?>
-						<a href="<?php echo esc_url( $this->plugin->get_support_url() ); ?>"target="_blank">
-							<?php esc_html_e( 'Get support', 'wp-license-client' ); ?>
+						<a href="<?php echo esc_url( $this->plugin->get_url() ); ?>" target="_blank" class="button-secondary">
+							<?php esc_html_e( 'Purchase', 'wp-license-client' ); ?>
 						</a>
-					<?php endif; ?>
+						<?php if ( $this->plugin->get_license_key_url() ) : ?>
+							<a href="<?php echo esc_url( $this->plugin->get_license_key_url() ); ?>" target="_blank">
+								<?php esc_html_e( 'Get license key', 'wp-license-client' ); ?>
+							</a>
+						<?php endif; ?>
+						<?php if ( $this->plugin->get_support_url() ) : ?>
+							<a href="<?php echo esc_url( $this->plugin->get_support_url() ); ?>" target="_blank">
+								<?php esc_html_e( 'Get support', 'wp-license-client' ); ?>
+							</a>
+						<?php endif; ?>
+					</span>
 				</div>
 			</div>
 		</div>
 		<?php
-	}
-
-	private static function find_my_menu_item( $menu_slug, $submenu_page_slug = false ) {
-
-		if ( ! is_admin() || ( defined( 'DOING_AJAX' ) && DOING_AJAX ) ) {
-			return false;
-		}
-
-		global $menu, $submenu;
-
-		$check_menu = $submenu_page_slug ? $submenu : $menu;
-
-		if ( empty( $check_menu[ $menu_slug ] ) ) {
-			return false;
-		}
-
-		if ( false === $submenu_page_slug ) {
-			return true;
-		}
-
-		foreach ( $check_menu[ $menu_slug ] as $i => $arr ) {
-
-			foreach ( $arr as $slug ) {
-
-				if ( $slug === $submenu_page_slug ) {
-					return true;
-				}
-			}
-		}
-
-		return false;
 	}
 }
